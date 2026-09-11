26f07ac7229539a84c14f17e099ecc522c2fd432
Xwilarg <xwilarg@protonmail.com>
2026-09-12 00:05:29 +0200

    Add sticker animation

---
+++ Assets/Scripts/UI/Sticker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Down2Jam.UI
+{
+    public class Sticker : MonoBehaviour
+    {
+        private float _timer = 2f;
+
+        private void Awake()
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 30f);
+        }
+
+        private void Update()
+        {
+            _timer -= Time.deltaTime;
+            if (_timer < 0f) _timer += 2f;
+
+            transform.rotation = Quaternion.Euler(0, 0, _timer < 1f ? -30f : 30f);
+        }
+    }
+}
