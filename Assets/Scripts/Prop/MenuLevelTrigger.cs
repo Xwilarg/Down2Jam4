47382e4136845716c2a553bc0515257fdb5aaf92
Xwilarg <xwilarg@protonmail.com>
2026-09-13 01:30:40 +0200

    Add basic main menu

---
+++ Assets/Scripts/Prop/MenuLevelTrigger.cs
@@ -0,0 +1,27 @@
+﻿using Down2Jam.Manager;
+using Down2Jam.Manager.Persistency;
+using Down2Jam.SO;
+using UnityEngine;
+
+namespace Down2Jam.Prop
+{
+    public class MenuLevelTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        private LevelInfo _level;
+
+        [SerializeField]
+        private LevelInfo _requirement;
+
+        private void Awake()
+        {
+            if (_requirement != null && PersistencyManager.Instance.SaveData.GetBestScore(_requirement.name) == 0)
+                Destroy(gameObject);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            MenuManager.Instance.LoadLevel(_level);
+        }
+    }
+}
