d95a916375e0588bb60e990ece3874c7cad3ec69
Xwilarg <xwilarg@protonmail.com>
2026-09-12 13:40:48 +0200

    Add debug manager

---
+++ Assets/Scripts/Manager/DebugManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Down2Jam.Manager
+{
+    [ExecuteInEditMode]
+    public class DebugManager : MonoBehaviour
+    {
+        [SerializeField]
+        private bool _resetStatic;
+
+        private void Update()
+        {
+            if (_resetStatic)
+            {
+                LoaderManager.CurrentLevel = null;
+                VNManager.SkipIntro = false;
+                _resetStatic = false;
+            }
+        }
+    }
+}
